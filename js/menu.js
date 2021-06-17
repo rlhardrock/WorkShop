1c3cd9f97b994305d45f6a9f2a2c0a64c5f6e539
Louis Rex <rlhardrock@gmail.com>
2021-06-17 09:45:49 -0500

    finished header (landing page) sticky-responsive768

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,13 @@
 let btnMenu = document.getElementById('btn-menu');
-let menu = document.getElementById('menu');
+let menu = document.getElementById('list-menu');
 
 btnMenu.addEventListener('click',function(){
     menu.classList.toggle('show');
 });
+
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth > 768){
+        menu.classList.remove('show');
+    }
+})
